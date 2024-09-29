3499a3575718a432755eb4ff68f04d90b122ee6b
julian <jdvs_g12@hotmail.com>
2024-09-29 11:54:24 -0500

    Fix style coverFlow anda add images

--- src/components/CarouselCoverFlow.jsx
+++ src/components/CarouselCoverFlow.jsx
@@ -2,31 +2,56 @@ import { useState, useEffect } from 'react'
 import '../styles/CoverFlow.scss'
 
 const images = [
-  'https://fastly.picsum.photos/id/9/5000/3269.jpg?hmac=cZKbaLeduq7rNB8X-bigYO8bvPIWtT-mh8GRXtU3vPc',
-  'https://fastly.picsum.photos/id/9/5000/3269.jpg?hmac=cZKbaLeduq7rNB8X-bigYO8bvPIWtT-mh8GRXtU3vPc',
-  'https://fastly.picsum.photos/id/9/5000/3269.jpg?hmac=cZKbaLeduq7rNB8X-bigYO8bvPIWtT-mh8GRXtU3vPc'
-  // 'https://fastly.picsum.photos/id/9/5000/3269.jpg?hmac=cZKbaLeduq7rNB8X-bigYO8bvPIWtT-mh8GRXtU3vPc',
-  // 'https://fastly.picsum.photos/id/9/5000/3269.jpg?hmac=cZKbaLeduq7rNB8X-bigYO8bvPIWtT-mh8GRXtU3vPc',
-  // 'https://fastly.picsum.photos/id/9/5000/3269.jpg?hmac=cZKbaLeduq7rNB8X-bigYO8bvPIWtT-mh8GRXtU3vPc'
+  './public/images/1.png',
+  './public/images/2.png',
+  './public/images/3.png',
+  './public/images/4.jpg'
 ]
 
 const Coverflow = () => {
   const [activeIndex, setActiveIndex] = useState(0)
   const totalImages = images.length
-
+  const windowWidth = window.innerWidth
+  const widthImages = windowWidth / images.length
   // Calcula el desplazamiento del wrapper para centrar el elemento activo
   const translateX =
     totalImages > 1
-      ? -(activeIndex * (300 + 30) - (window.innerWidth / 2 - 150)) // Ajusta según $coverflow-item-width y $coverflow-item-gap
+      ? -(
+          activeIndex * widthImages +
+          30 -
+          (windowWidth / 2 - widthImages / 2 + 50)
+        )
       : 0 // No mover si solo hay una imagen
+
+  const cycleSelection = (arr, interval) => {
+    setInterval(() => {
+      // Remove the last element and move it to the start to "rotate" the array
+      const newArr = [...arr]
+      const lastItem = newArr.pop()
+      if (lastItem) {
+        newArr.unshift(lastItem)
+      }
+
+      // Ensure only the third item (index 2) has isSelected: true, others false
+      const updatedItems = newArr.map((item, index) => ({
+        ...item,
+        isSelected: index === 2 // Only the item at index 2 should be selected
+      }))
+      console.log(arr)
+
+      // Update the array with the modified array
+      arr = updatedItems
+    }, interval)
+  }
+
+  cycleSelection(images, 1500)
 
   // Automáticamente cambia la imagen activa cada 3 segundos si hay más de una imagen
   useEffect(() => {
     if (totalImages > 1) {
       const interval = setInterval(() => {
-        setActiveIndex(prevIndex => (prevIndex + 1) % totalImages) // Avanza al siguiente índice
-      }, 1500) // Cambiar cada 3 segundos
-
+        setActiveIndex(prevIndex => (prevIndex + 1) % totalImages)
+      }, 1500)
       return () => clearInterval(interval) // Limpia el intervalo al desmontar el componente
     }
   }, [totalImages])
